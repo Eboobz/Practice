81861f43055ad08a339d9ca6c75844abecef0e6e
Eboobz <echianuebube@yahoo.com>
2023-08-02 15:55:57 +0100

    good

---
+++ intchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+//Hey, i'm trying a lot today
+void main()
+{
+int a = 37, b, c=100, sum;
+sum = d + e
+printf("hello world");
+putchar('\n');
+printf("I am %d years old", a);
+putchar('\n');
+printf("%c is your class", 'c');
+putchar('\n');
+printf("What is the value for %d", 'b');
+putchar('\n');
+printf("Enter two numbers:");
+scanf(%d %d, &d, &e);
+printf("%d", sum);
+}
