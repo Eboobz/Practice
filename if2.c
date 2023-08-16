d4662bf1f8a66fab61c1a9b773727eadec6b4fca
Eboobz <echianuebube@yahoo.com>
2023-08-16 13:26:14 +0100

    good

---
+++ if2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+//if and else
+void main()
+{
+int a, char b, c, d;
+printf("Enter a character");
+scanf("&c", b, c, d);
+if (b >= a && b<= z; b++)
+printf("small letter %c", b);
+else if (c >= A && c <= Z; c++)
+printf("capital letter %c", c);
+else if
+printf("symbol %c", d);
+else
+printf("Enter a number");
+scanf("%d", a);
+if (a >= 0 && a <= 9)
+printf("number %d", a);
+else
+printf("End");
+}
